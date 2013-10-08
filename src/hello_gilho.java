a32a451a966619af87bb7c2f12a30634d6391aa5
piece1990 <piece1990@naver.com>
2013-10-09 01:19:26 +0900

    20094003
    20094003

--- src/hello_gilho.java
+++ src/hello_gilho.java
@@ -1,9 +1,6 @@
 
-public class hello_gilho {
-
+public class Hello_gilho {
 	public static void main(String[] args) {
-		System.out.println("hello git");
-
+		System.out.println("hello world");
 	}
-
 }
